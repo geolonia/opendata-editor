46b607bc8fea0b891055a9f60443630e512a78ff
Junya Ishihara <junya@champierre.com>
2023-03-08 12:09:38 +0900

    ReactGridで作成したテーブルのフィールドを編集できるようにする。

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactGrid, Column, Row } from "@silevis/reactgrid";
+import { ReactGrid, Column, Row, CellChange, TextCell } from "@silevis/reactgrid";
 import "@silevis/reactgrid/styles.css";
 
 interface Props {
@@ -11,15 +11,32 @@ interface TableData {
   [key: string]: string;
 }
 
+const applyChangesToData = (
+  changes: CellChange<TextCell>[],
+  prevTableData: TableData[]
+): TableData[] => {
+  changes.forEach((change) => {
+    const index = change.rowId as number;
+    const fieldName = change.columnId;
+    prevTableData[index][fieldName] = change.newCell.text;
+  });
+  return [...prevTableData];
+};
+
 const Component = (props: Props) => {
-  const properties = props.data.features.map((feature) => { return feature.properties })
-  const [tableData] = React.useState<TableData[] | any>(properties);
+  const properties = props.data.features.map((feature) => { return feature.properties as TableData })
+  const [tableData, setTableData] = React.useState<TableData[]>(properties);
+
+  const handleChanges = (changes: CellChange[]) => {
+    const textCellChanges = changes.filter(x => x.type === 'text') as CellChange<TextCell>[];
+    setTableData((prevTableData) => applyChangesToData(textCellChanges, prevTableData));
+  };
 
   const headerRow: Row = {
     rowId: "header",
     cells: properties[0] ? Object.keys(properties[0]).map((key) => {
       return { type: "header", text: key };
-    }) : [{ type: "header", text: "" }]
+    }) : []
   };
 
   const getRows = (tableData: TableData[]): Row[] => [
@@ -30,9 +47,9 @@ const Component = (props: Props) => {
     }))
   ];
 
-  const getColumns = (): Column[] | null => properties[0] && Object.keys(properties[0]).map((key) => {
+  const getColumns = (): Column[] => properties[0] ? Object.keys(properties[0]).map((key) => {
     return { columnId: key, width: 150 };
-  });
+  }) : [];
   
   const rows = getRows(tableData);
   const columns = getColumns();
@@ -40,9 +57,7 @@ const Component = (props: Props) => {
     <div className="main">
       <div className="container">
         <p>{props.data.features.length}件のデータが登録されています。</p>
-        {columns && rows &&
-          <ReactGrid rows={rows} columns={columns} />
-        }
+        <ReactGrid rows={rows} columns={columns} onCellsChanged={handleChanges}/>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <Home />
 );
 
 // If you want to start measuring performance in your app, pass a function

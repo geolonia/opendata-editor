87e498571434999b95c8e3c26228d24d7b9af1bd
Junya Ishihara <junya@champierre.com>
2023-04-13 19:58:32 +0900

    useEffect の依存変数を整理。

--- src/Map.tsx
+++ src/Map.tsx
@@ -33,6 +33,15 @@ const Component = (props: Props) => {
   const [map, setMap] = React.useState();
   const [draggableMarker, setDraggableMarker] = React.useState(null);
 
+  const editMode = props.editMode;
+  const setEditMode = props.setEditMode;
+  const selectedRowId = props.selectedRowId;
+  const setSelectedRowId = props.setSelectedRowId;
+  const features = props.features;
+  const setFeatures = props.setFeatures;
+  const fitBounds = props.fitBounds;
+  const setFitBounds = props.setFitBounds;
+
   React.useEffect(() => {
     if (!map) {
       const map = new window.geolonia.Map({
@@ -55,31 +64,31 @@ const Component = (props: Props) => {
 
       map.on('click', 'custom-geojson-circle-points', (e: any) => {
         const id = e.features[0].properties['id'];
-        props.setEditMode(false);
-        props.setSelectedRowId(id);
+        setEditMode(false);
+        setSelectedRowId(id);
       });
     }
-  }, [mapContainer, props.features, props, map])
+  }, [mapContainer, map, setEditMode, setSelectedRowId])
 
   React.useEffect(() => {
     if (simpleStyle) {
-      const string = Papa.unparse(props.features);
+      const string = Papa.unparse(features);
       const geojson = csv2geojson(string);
 
-      if (props.fitBounds) {
+      if (fitBounds) {
         // @ts-ignore
         simpleStyle.updateData(geojson).fitBounds();
-        props.setFitBounds(false);
+        setFitBounds(false);
       } else {
         // @ts-ignore
         simpleStyle.updateData(geojson)
       }
     }
-  }, [simpleStyle, props.features])
+  }, [simpleStyle, features, fitBounds, setFitBounds])
 
   React.useEffect(() => {
-    if (map && props.selectedRowId !== null) {
-      const selectedFeature = props.features.find((feature) => feature.id === props.selectedRowId);
+    if (map && selectedRowId !== null) {
+      const selectedFeature = features.find((feature) => feature.id === selectedRowId);
 
       // @ts-ignore
       let center = map.getCenter();
@@ -92,19 +101,18 @@ const Component = (props: Props) => {
         draggableMarker.remove();
       }
 
-      if (props.editMode) {
+      if (editMode) {
         const marker = new window.geolonia.Marker({ draggable: true }).setLngLat(center).addTo(map);
         setDraggableMarker(marker);
 
         marker.on('dragend', () => {
-          const features = props.features
-          const feature = features.find((feature) => feature['id'] === props.selectedRowId);
+          const feature = features.find((feature) => feature['id'] === selectedRowId);
           const lngLat = marker.getLngLat();
 
           // 新規データ追加の場合
           if (!feature) {
-            const latField = document.querySelector(`tr#table-data-${props.selectedRowId} td:nth-child(3) input`);
-            const lngField = document.querySelector(`tr#table-data-${props.selectedRowId} td:nth-child(4) input`);
+            const latField = document.querySelector(`tr#table-data-${selectedRowId} td:nth-child(3) input`);
+            const lngField = document.querySelector(`tr#table-data-${selectedRowId} td:nth-child(4) input`);
 
             if (latField && lngField) {
               //@ts-ignore
@@ -117,15 +125,15 @@ const Component = (props: Props) => {
 
           // 既存データ編集の場合
           if (!window.confirm(`「${feature?.name}」の位置情報を変更しても良いですか?`)) {
-            props.setEditMode(false);
+            setEditMode(false);
             marker.remove();
             return;
           }
 
           feature.longitude = lngLat.lng.toString();
           feature.latitude = lngLat.lat.toString();
-          props.setFeatures([...features]);
-          props.setEditMode(false);
+          setFeatures([...features]);
+          setEditMode(false);
           marker.remove();
         });
       }
@@ -136,7 +144,7 @@ const Component = (props: Props) => {
         zoom: 17
       })
     }
-  }, [map, props.editMode, props.selectedRowId])
+  }, [map, editMode, selectedRowId, setEditMode, features, setFeatures])
 
   return (
     <>

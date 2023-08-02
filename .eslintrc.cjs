e5ad6efd244e653791971b35f98aa00b63c43415
Jumpei Ogawa <phanect@users.noreply.github.com>
2023-08-02 20:02:48 +0900

    chore: disable unwanted ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,4 +10,9 @@ module.exports = {
     project: './tsconfig.json',
   },
   ignorePatterns: ['/dist/*'],
+
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
 };

6b28522c19cc5b5aa762df84379bb8900619ac8f
Jumpei Ogawa <phanect@users.noreply.github.com>
2023-08-04 15:07:12 +0900

    feat: allow loading data from prop

--- lib/OpenDataEditor.tsx
+++ lib/OpenDataEditor.tsx
@@ -5,6 +5,7 @@ import Download from './Download';
 
 import { Buffer } from 'buffer';
 import Encoding from 'encoding-japanese';
+import Papa from 'papaparse';
 
 import Map from './Map';
 import Uploader from './Uploader';
@@ -14,7 +15,11 @@ import { addIdToFeatures } from './utils/add-id-to-features';
 import './OpenDataEditor.scss';
 import { Row, csv2rows } from './utils/csv2geojson';
 
-const OpenDataEditor = (): JSX.Element => {
+type Props = {
+  data?: string[][];
+};
+
+const OpenDataEditor = ({ data }: Props): JSX.Element => {
   const [ features, setFeatures ] = React.useState<Row[]>([]);
   const [ filename, setFilename ] = React.useState<string>('');
   const [ editMode, setEditMode ] = React.useState(false);
@@ -22,30 +27,43 @@ const OpenDataEditor = (): JSX.Element => {
   const [ selectedRowId, setSelectedRowId ] = React.useState<string | null>(null);
   const [ selectedOn, setSelectedOn ] = React.useState<string | null>(null);
 
+  const hideUploader = () => {
+    const el = document.querySelector('.uploader') as HTMLElement;
+    el.style.display = 'none';
+  };
+
   React.useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const path = params.get('data');
-
-    if (!path) return;
-
-    const filename = path.split('/').pop() || '';
-    setFilename(filename);
-    fetch(path)
-      .then((response) => response.arrayBuffer())
-      .then((data) => {
-        const buffer = Buffer.from(data);
-        const unicodeData = Encoding.convert(buffer, {
-          to: 'UNICODE',
-          from: 'AUTO',
-          type: 'string',
-        });
-        const el = document.querySelector('.uploader') as HTMLElement;
-        el.style.display = 'none';
-        const features = csv2rows(unicodeData);
-        setFitBounds(true);
-        setFeatures(addIdToFeatures(features));
+    if (data) {
+      const csv = Papa.unparse(data);
+      const { data: formattedData } = Papa.parse<Row>(csv, {
+        header: true,
       });
-  }, []);
+      setFeatures(addIdToFeatures(formattedData));
+      hideUploader();
+    } else if (location.search) {
+      const params = new URLSearchParams(location.search);
+      const path = params.get('data');
+
+      if (!path) return;
+
+      const filename = path.split('/').pop() || '';
+      setFilename(filename);
+      fetch(path)
+        .then((response) => response.arrayBuffer())
+        .then((data) => {
+          const buffer = Buffer.from(data);
+          const unicodeData = Encoding.convert(buffer, {
+            to: 'UNICODE',
+            from: 'AUTO',
+            type: 'string',
+          });
+          hideUploader();
+          const features = csv2rows(unicodeData);
+          setFitBounds(true);
+          setFeatures(addIdToFeatures(features));
+        });
+    }
+  }, [data]);
 
   return (
     <div className="main">

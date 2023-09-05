0da1e66be3a116db039ab7c597fea7b8fccc34a8
Jumpei Ogawa <phanect@users.noreply.github.com>
2023-09-05 18:17:58 +0900

    refactor: extract process as `getLatLngColumnNames()`

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,2 +1,15 @@
+import { Feature } from '../types';
+
 export const dedupe = <T>(arr: T[]): T[] =>
   arr.filter((item, i) => arr.indexOf(item) === i);
+
+export const getLatLngColumnNames = (row: Feature | Feature[]): { latColumnName?: string, lngColumnName?: string } => {
+  const latColumnCandidates = [ '緯度', 'lat', 'latitude', '緯度（10進法）', '緯度(10進法)'] as const;
+  const lngColumnCandidates = [ '経度', 'lng', 'longitude', '経度（10進法）', '経度(10進法)' ] as const;
+  const columnNames = Object.keys(Array.isArray(row) ? row[0] : row);
+
+  return {
+    latColumnName: latColumnCandidates.find((latColumnCandidate) => columnNames.includes(latColumnCandidate)),
+    lngColumnName: lngColumnCandidates.find((lngColumnCandidate) => columnNames.includes(lngColumnCandidate)),
+  };
+};

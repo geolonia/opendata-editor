ce7aa0118ae07b0bb5b79105c451970942dd0795
Jumpei Ogawa <phanect@users.noreply.github.com>
2023-09-07 19:34:36 +0900

    feat: restore "add row" (データを追加) button

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -14,13 +14,17 @@ export const getRowById = (rows: Feature[], rowId: string): { row: Feature, rowI
   return { row, rowIdx };
 };
 
-export const getLatLngColumnNames = (row: Feature | Feature[]): { latColumnName?: string, lngColumnName?: string } => {
+export const getLatLngColumnNames = (row: Feature | Feature[]): { latColumnName?: string, lngColumnName?: string, latIndex: number, lngIndex: number } => {
   const latColumnCandidates = [ '緯度', 'lat', 'latitude', '緯度（10進法）', '緯度(10進法)'] as const;
   const lngColumnCandidates = [ '経度', 'lng', 'longitude', '経度（10進法）', '経度(10進法)' ] as const;
   const columnNames = Object.keys(Array.isArray(row) ? row[0] : row);
 
+  const latIndex = latColumnCandidates.findIndex((latColumnCandidate) => columnNames.includes(latColumnCandidate));
+  const lngIndex = lngColumnCandidates.findIndex((lngColumnCandidate) => columnNames.includes(lngColumnCandidate));
   return {
-    latColumnName: latColumnCandidates.find((latColumnCandidate) => columnNames.includes(latColumnCandidate)),
-    lngColumnName: lngColumnCandidates.find((lngColumnCandidate) => columnNames.includes(lngColumnCandidate)),
+    latColumnName: latColumnCandidates[latIndex],
+    lngColumnName: lngColumnCandidates[lngIndex],
+    latIndex,
+    lngIndex,
   };
 };

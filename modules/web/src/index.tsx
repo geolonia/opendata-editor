2a3cee6f3b3fb15a7f82ad3839b9a6ab013bd0c4
Jumpei Ogawa <git-geolonia@phanective.org>
2023-12-06 15:18:35 +0900

    fix: expose missing stylesheet

--- modules/web/src/index.tsx
+++ modules/web/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { OpenDataEditor } from '@geolonia/opendata-editor';
+import '@geolonia/opendata-editor/style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,

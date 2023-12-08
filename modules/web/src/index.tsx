c7f82cb02169e99bda495329c92ceffeb3ff352d
Jumpei Ogawa <git-geolonia@phanective.org>
2023-12-08 19:38:03 +0900

    feat: add `initOpenDataEditor()`

--- modules/web/src/index.tsx
+++ modules/web/src/index.tsx
@@ -4,9 +4,25 @@ import './index.css';
 import { OpenDataEditor } from '@geolonia/opendata-editor';
 import '@geolonia/opendata-editor/style.css';
 
-const root = createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <OpenDataEditor />,
-);
+export const initOpenDataEditor = (element: string | HTMLElement | null): void => {
+  let el;
+
+  if (typeof element === 'string') {
+    el = document.querySelector(element);
+
+    if (!el) {
+      throw new Error(`Element ${element} not found`);
+    }
+  } else if (element instanceof HTMLElement) {
+    el = element;
+  } else if (element === null) {
+    throw new Error('Element not found (null is given as an argument)');
+  } else {
+    throw new Error(`type "${typeof element}" is given as an argument. Element must be a string or an HTMLElement`);
+  }
+
+  const root = createRoot(el);
+  root.render(
+    <OpenDataEditor />,
+  );
+};

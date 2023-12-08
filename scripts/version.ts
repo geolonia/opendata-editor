8275482c55f4d41fbb6917f94eb0de5551317a0a
Jumpei Ogawa <git-geolonia@phanective.org>
2023-12-08 21:42:03 +0900

    chore: add a new npm script: version

--- scripts/version.ts
+++ scripts/version.ts
@@ -3,9 +3,14 @@ import { join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { version as newVersion } from '../lerna.json';
 import packageJsonRoot from '../package.json';
+import packageJsonTemplate from '../modules/create/template/package.json';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 packageJsonRoot.version = newVersion;
+packageJsonTemplate.dependencies['@geolonia/opendata-editor-web'] = `^${newVersion}`;
 
-await writeFile(join(__dirname, '../package.json'), `${JSON.stringify(packageJsonRoot, null, 2)}\n`);
+await Promise.all([
+  writeFile(join(__dirname, '../package.json'), `${JSON.stringify(packageJsonRoot, null, 2)}\n`),
+  writeFile(join(__dirname, '../modules/create/template/package.json'), `${JSON.stringify(packageJsonTemplate, null, 2)}\n`),
+]);

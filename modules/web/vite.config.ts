b1e19d3bc71214ee00d57c5389f0719098689e9e
Jumpei Ogawa <git-geolonia@phanective.org>
2023-12-25 21:12:17 +0900

    fix: add missing `__dirname`

--- modules/web/vite.config.ts
+++ modules/web/vite.config.ts
@@ -1,6 +1,9 @@
 import { resolve } from 'path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   build: {
